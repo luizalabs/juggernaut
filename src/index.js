1db6b91fa77fb3584c5eff6c1e8060dec55613f4
Stanley Gomes <stanleygomesdasilva@gmail.com>
2019-02-20 15:41:11 -0300

    Ajustes nos diretorios para ficar num padrao mais adequado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './views/App';
+import App from './views/app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
