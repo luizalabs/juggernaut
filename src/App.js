3737a1e731a9ce6a49afc6932684514028b5a678
Marlon Maxwel <marlonmaxwel@softbox.com.br>
2019-03-19 09:28:53 -0300

    Add app structure and styled components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './commons/styles/global';
+import Main from './containers/Main';
+import theme from './commons/constants/theme';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Fragment>
+      <GlobalStyle />
+      <Main />
+    </Fragment>
+  </ThemeProvider>
+);
 
 export default App;
---
+++ src/containers/Main/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const ContainerStyled = styled.div`
+  color: ${({ theme }) => theme.primary};
+`;
+
+export default ContainerStyled;
