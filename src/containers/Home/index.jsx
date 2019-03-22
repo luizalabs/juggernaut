9582228baa0cc797f44fec1e8edb22d96d1cf173
Marlon Maxwel <marlonmleite@gmail.com>
2019-03-22 14:16:40 -0300

    Add react router dom and routes (#41)
    * [FE] Add react router dom
    
    * Update routes structure

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <h1>Home page</h1>;
+
+export default Home;
--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import routes from '../../routes';
 import ContainerStyled from './styled';
 
 const Main = () => (
   <ContainerStyled>
-    <p>Hello!</p>
+    <Switch>
+      {routes.map(route => (
+        <Route
+          key={route.path}
+          path={route.path}
+          exact={route.exact}
+          component={route.main}
+        />
+      ))}
+    </Switch>
   </ContainerStyled>
 );
 
