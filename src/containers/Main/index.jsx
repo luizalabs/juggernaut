7aaf2ff31e4336aaa7b14fe3cd6aedd873820daf
Marlon Maxwel <marlonmaxwel@softbox.com.br>
2019-03-22 09:06:38 -0300

    [FE] Add code splitting based in routes

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from '../../routes';
+import Loading from '../../components/Loading';
 import ContainerStyled from './styled';
 
 const Main = () => (
   <ContainerStyled>
-    <Switch>
-      {routes.map(route => (
-        <Route
-          key={route.path}
-          path={route.path}
-          exact={route.exact}
-          component={route.main}
-        />
-      ))}
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        {routes.map(route => (
+          <Route
+            key={route.path}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        ))}
+      </Switch>
+    </Suspense>
   </ContainerStyled>
 );
 
