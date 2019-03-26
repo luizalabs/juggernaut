b91219f4428e61611573f0b5012b33ed3f09bc9e
Marcony Neves <marconyneves@softbox.com.br>
2019-03-26 09:58:41 -0300

    fix(eslint): Removido regras e adequado itens

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next'
+import PropTypes from 'prop-types'
 
 const Home = ({ t }) => <h1>{ t('general.welcome') }</h1>
+
+Home.propTypes = {
+  t: PropTypes.func
+}
 
 export default withTranslation()(Home)
--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,14 +1,14 @@
-import React, { Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import routes from '../../routes';
-import Loading from '../../components/Loading';
-import ContainerStyled from './styled';
+import React, { Suspense } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import routes from '../../routes'
+import Loading from '../../components/Loading'
+import ContainerStyled from './styled'
 
 const Main = () => (
   <ContainerStyled>
     <Suspense fallback={<Loading />}>
       <Switch>
-        {routes.map(route => (
+        {routes.map((route) => (
           <Route
             key={route.path}
             path={route.path}
@@ -19,6 +19,6 @@ const Main = () => (
       </Switch>
     </Suspense>
   </ContainerStyled>
-);
+)
 
-export default Main;
+export default Main
