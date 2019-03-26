f6c75076eb39fb872a48e4e559d7eb569000243a
Rafael Alves <rafaelalves>
2019-03-26 15:14:25 -0300

    Inserindo Mateiral - Ui

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { withTranslation } from 'react-i18next'
+import Button from '@material-ui/core/Button'
 
-const Home = ({ t }) => <h1>{ t('general.welcome') }</h1>
+const Hello = (t) => {
+  return (
+    <div>
+      <Button variant="contained">
+        Hello World
+      </Button>
+    </div>
+  )
+}
+// const Home = ({ t }) => <h1 >{ t('general.welcome') }</h1>
 
-export default withTranslation()(Home)
+export default withTranslation()(Hello)
