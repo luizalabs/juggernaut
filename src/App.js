f6f8ed5623177a04085d9bb6f052b81972a13058
Rafael Alves <rafaelalves>
2019-04-01 10:52:28 -0300

    inserindo CssBaseline e retornando ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { MuiThemeProvider } from '@material-ui/core/styles'
-import './commons/i18n'
+import CssBaseline from '@material-ui/core/CssBaseline';
 import GlobalStyle from './commons/styles/global'
 import Main from './containers/Main'
 import theme from './commons/constants/theme'
+import './commons/i18n'
 
 const App = () => (
+
   <MuiThemeProvider theme={theme}>
-    <Fragment>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </Fragment>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <CssBaseline />
+        <GlobalStyle />
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Fragment>
+    </ThemeProvider>
   </MuiThemeProvider>
 )
 
