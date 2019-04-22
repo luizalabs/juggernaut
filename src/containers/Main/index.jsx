2b616f32945ec0f005cab3a2d17bb4bb45ff4f11
Lucas Vilaboim <lucas@vilaboim.com>
2019-04-22 14:17:17 -0300

    feat(alias): add alias config (#66)

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import routes from '../../routes'
-import Loading from '../../components/Loading'
+import Loading from 'components/Loading'
 import StyledContainer from './style'
 
 const Main = () => (
