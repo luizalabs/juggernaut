70281c517bc68b6e1f4ce09558b79f3e1e97e510
Marcony Neves <desenvolvedor@me.com>
2019-04-26 20:16:50 -0300

    chore(move): Move juggernaut to a container pattern

--- src/containers/Juggernaut/index.jsx
+++ src/containers/Juggernaut/index.jsx
@@ -2,16 +2,17 @@ import React, { Fragment } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyle from './global'
+import GlobalStyle from 'commons/styles/global'
 import PropTypes from 'prop-types'
+import 'commons/i18n'
 
-const Juggernaut = ({ children: Children, theme }) => (
+const Juggernaut = ({ children, theme }) => (
   <MuiThemeProvider theme={theme}>
     <ThemeProvider theme={theme}>
       <Fragment>
         <CssBaseline />
         <GlobalStyle />
-        {Children}
+        {children}
       </Fragment>
     </ThemeProvider>
   </MuiThemeProvider>
