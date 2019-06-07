44e565d0af70ea4a97bd987b9eb2e3dc978011b4
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2019-06-07 17:39:20 -0300

    feat: add docz

---
+++ doczrc.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'Juggernaut',
+  description: 'An unstoppable boilerplate',
+  themeConfig: {
+    colors: {
+      primary: 'tomato'
+    },
+    styles: {
+      h1: `
+        font-size: 80px;
+        margin-bottom: 10px;
+      `
+    }
+  }
+}
