254d356b85f4332332717e89ec163df755ffdb63
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2019-06-21 09:48:05 -0300

    Feat/docz (#74)
    * feat: add docz
    
    * add the index page mdx
    
    * fix: Alter configs to deploy in github pages
    
    * fix: add hashRouter

---
+++ doczrc.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Juggernaut',
+  description: 'An unstoppable boilerplate',
+  dest: './docs',
+  hashRouter: true,
+  themeConfig: {
+    colors: {
+      primary: 'tomato'
+    },
+    styles: {
+      h1: `
+        font-size: 80px;
+        margin-bottom: 10px;
+      `
+    }
+  }
+}
