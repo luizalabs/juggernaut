34d598047954f6539c026933835af19d76222908
Marlon Maxwel <marlonmleite@gmail.com>
2019-08-01 09:59:53 -0300

    Update material-ui (#76)
    * Atualização da biblioteca material-ui
    
    * Update lock

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider } from '@material-ui/styles'
 import Juggernaut from 'containers/Juggernaut'
 import theme from 'commons/constants/theme'
 import Main from 'containers/Main'
 
 const App = () => (
-  <Juggernaut theme={theme}>
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>
-  </Juggernaut>
+  <StylesProvider injectFirst>
+    <Juggernaut theme={theme}>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </Juggernaut>
+  </StylesProvider>
 )
 
 export default App
