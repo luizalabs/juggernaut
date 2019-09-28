c3e4a7d4111446a6fbf75002ef42a2a3935759df
Bus42 <gbrewton1980@gmail.com>
2019-09-28 13:26:41 -0600

    Implements circular loading component, console msg

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,6 +1,43 @@
 import React from 'react'
-import LoadingStyled from './styled'
+import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
-const Loading = () => <LoadingStyled>Loading...</LoadingStyled>
+const useStyles = makeStyles(theme => ({
+  progress: {
+    margin: theme.spacing(2)
+  }
+}))
+
+// positions loader in the center of the containing div element
+// enlarges animation for better visibility
+const loaderStyle = {
+  position: 'absolute',
+  top: '0',
+  bottom: '0',
+  left: '0',
+  right: '0',
+  margin: 'auto',
+  transform: 'scale(2)'
+}
+
+const name = 'Loading Component'
+
+// sizes wrapper to viewport dimensions
+const wrapperStyle = {
+  width: '100vw',
+  height: '100vh'
+}
+
+const Loading = () => {
+  console.groupCollapsed(name)
+  console.log(`${name} implemented by %cGreg Brewton www.Studio42Dev.com %cfor %cHacktoberfest 2019`, 'color: #007AAF','color: white', 'color: #FF7145')
+  console.groupEnd(name)
+  const classes = useStyles()
+  return (
+    <div style={wrapperStyle}>
+      <CircularProgress style={loaderStyle} className={classes.progress} />
+    </div>
+  )
+}
 
 export default Loading
