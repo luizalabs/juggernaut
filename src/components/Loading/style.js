c3ed65d6b10dc31a7599a2d6cdc99e872f00a604
Greg Brewton <gbrewton1980@gmail.com>
2019-10-08 11:24:57 -0600

    refactors to use flexbox

--- src/components/Loading/style.js
+++ src/components/Loading/style.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components'
 
 const LoadingStyled = styled.div`
-position: absolute;
-height: 1px;
-width: 1px;
-top: 0;
-bottom: 0;
-left: 0;
-right: 0;
-margin: auto;
+width: 100vw;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
 `
 
 export default LoadingStyled
