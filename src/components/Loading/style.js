a9bab200f2a8b528e6bf47ef63f9c4d9960b91bc
Greg Brewton <gbrewton1980@gmail.com>
2019-10-09 08:14:53 -0600

    Adds property indentation, adjusts CSS

--- src/components/Loading/style.js
+++ src/components/Loading/style.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 const LoadingStyled = styled.div`
-width: 100vw;
-height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
 export default LoadingStyled
