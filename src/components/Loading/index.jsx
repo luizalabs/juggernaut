27d99e720c22a04b68a31c72286a5b37a18258a0
Marlon Maxwel <marlonmleite@gmail.com>
2019-11-04 10:54:40 -0300

    Fix

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import LoadingStyled from './style'
 
-const Loading = () => <LoadingStyled><CircularProgress/></LoadingStyled>
+const Loading = () => (
+  <LoadingStyled>
+    <CircularProgress/>
+  </LoadingStyled>
+)
 
 export default Loading
