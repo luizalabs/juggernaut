4594755cbb331a07e2b6598f8eec7d7fd5f713f0
Lucas <lucas@vilaboim.com>
2020-10-29 18:07:41 -0300

    feat(i18n): add useTranslation hook

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Trans, withTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import logo from 'assets/images/juggernaut.gif'
 import { StyledContainer, StyledTitle, StyledSubtitle, StyledImage } from './style'
 
-const Home = ({ t }) => (
-  <StyledContainer>
-    <StyledImage
-      src={logo}
-      alt="Gif by Reuno"
-      title="Gif by Reuno"
-    />
-    <StyledTitle color="primary" component="h1" variant="h4" gutterBottom>
-      <Trans>{t('general.welcome')}</Trans>
-    </StyledTitle>
-    <StyledSubtitle color="primary" component="h3" variant="h5">
-      { t('general.description') }
-    </StyledSubtitle>
-  </StyledContainer>
-)
+const Home = () => {
+  const { t } = useTranslation()
+
+  return (
+    <StyledContainer>
+      <StyledImage
+        src={logo}
+        alt="Gif by Reuno"
+        title="Gif by Reuno"
+      />
+      <StyledTitle color="primary" component="h1" variant="h4" gutterBottom>
+        <Trans>{t('general.welcome')}</Trans>
+      </StyledTitle>
+      <StyledSubtitle color="primary" component="h3" variant="h5">
+        { t('general.description') }
+      </StyledSubtitle>
+    </StyledContainer>
+  )
+}
 
 Home.propTypes = {
   t: PropTypes.func.isRequired
 }
 
-export default withTranslation()(Home)
+export default Home
