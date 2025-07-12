4176ab8a054f729b56d27c54258774f4be061672
stanley <stanleygomesdasilva@gmail.com>
2025-07-12 13:42:19 -0300

    chore: mover geracao de release para o ci/cd

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,14 +8,8 @@ module.exports = {
         'feat', // A new feature
         'fix', // A bug fix
         'docs', // Documentation only changes
-        'style', // Changes that do not affect the meaning of the code
-        'refactor', // A code change that neither fixes a bug nor adds a feature
-        'perf', // A code change that improves performance
         'test', // Adding missing tests or correcting existing tests
-        'build', // Changes that affect the build system or external dependencies
-        'ci', // Changes to our CI configuration files and scripts
         'chore', // Other changes that don't modify src or test files
-        'revert', // Reverts a previous commit
       ],
     ],
     'subject-case': [
